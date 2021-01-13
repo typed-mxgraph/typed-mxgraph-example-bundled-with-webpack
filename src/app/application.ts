d3f04c7202be3f1d66ebbba64313d80909c4e19b
鸿则 <hungtcs@163.com>
2021-01-13 23:33:04 +0800

    feat: update mxgraph import helper

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,14 +1,15 @@
-import { mxClient, mxGraph } from './mxgraph';
+import mx from './mxgraph';
+import { mxGraph, mxGraphModel } from 'mxgraph';
 
 export class Application {
 
   constructor(container: HTMLElement) {
-    if(mxClient.isBrowserSupported()) {
+    if(mx.mxClient.isBrowserSupported()) {
       console.log('Yes! Yes!');
     }
 
-    const graph = new mxGraph(container);
-    const model = graph.getModel();
+    const graph: mxGraph = new mx.mxGraph(container);
+    const model: mxGraphModel = graph.getModel();
     model.beginUpdate();
     try {
       graph.insertVertex(graph.getDefaultParent(), '', 'TEST', 0, 0, 100, 100);
--- src/app/mxgraph.ts
+++ src/app/mxgraph.ts
@@ -2,9 +2,7 @@ import factory from 'mxgraph';
 
 (window as any)['mxBasePath'] = 'assets/mxgraph';
 
-const { mxGraph, mxClient, mxRectangleShape } = factory({
+export default factory({
   // not working see https://github.com/jgraph/mxgraph/issues/479
   mxBasePath: 'assets/mxgraph',
 });
-
-export { mxGraph, mxClient, mxRectangleShape };
